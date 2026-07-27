b0690ac859cad8939c654f8528f231204b83877f
akachi11 <ogogorchimadika6@gmail.com>
2026-07-28 00:04:46 +0100

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1 className={'text-red-800'}>Test text</h1>
+        </div>
+    );
+};
+
+export default App;
