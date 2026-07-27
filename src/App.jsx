b2376b5fa553235c585304bf6bd09cace49b3e43
akachi11 <ogogorchimadika6@gmail.com>
2026-07-28 00:14:11 +0100

    Implement Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import NavBar from "./components/Navbar.jsx";
 
 const App = () => {
     return (
         <div>
-            <h1 className={'text-red-800'}>Test text</h1>
+           <NavBar />
         </div>
     );
 };
