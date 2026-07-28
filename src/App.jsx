bb0e2fd32d9f6bec49d59e91523ecbf8c493a311
akachi11 <ogogorchimadika6@gmail.com>
2026-07-28 18:42:07 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import NavBar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
+import ProductViewer from "./components/ProductViewer.jsx";
+import Showcase from "./components/Showcase.jsx";
 
 const App = () => {
     return (
         <div>
            <NavBar />
             <Hero />
+            <ProductViewer />
+            <Showcase />
         </div>
     );
 };
